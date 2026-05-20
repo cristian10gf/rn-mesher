8d9f6e0d0246f60c6598a8047da53fceacc62e4f
Daniel Jose Romero Martinez <djromer@gmail.com>
2026-05-19 21:17:22 -0500

    change on apple config

--- client/rn-arkit-replacement/ios/UniWhereLiDAR/Native/RNLiDARBridgeModule.m
+++
@@ -1,9 +0,0 @@
-#import <React/RCTBridgeModule.h>
-
-@interface RCT_EXTERN_MODULE(RNLiDARBridgeModule, NSObject)
-
-RCT_EXTERN_METHOD(startScan:(id)config resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
-RCT_EXTERN_METHOD(stopScan:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
-RCT_EXTERN_METHOD(exportMesh:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
-
-@end
--- client/rn-arkit-replacement/ios/UniWhereLiDAR/Native/RNLiDAREventEmitter.m
+++
@@ -1,8 +0,0 @@
-#import <React/RCTEventEmitter.h>
-#import <React/RCTBridgeModule.h>
-
-@interface RCT_EXTERN_MODULE(RNLiDAREventEmitter, RCTEventEmitter)
-
-RCT_EXTERN_METHOD(sendMeshUpdate:(NSDictionary *)data)
-
-@end
